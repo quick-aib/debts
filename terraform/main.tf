299b1f7f65bc807eaf716d732512e7214437034f
ravi <ravi@quick.com>
2023-02-15 08:08:25 +0530

    updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-
-  required_version = ">= 1.2.0"
-}
-
-provider "aws" {
-  region  = "us-west-2"
-}
 
 resource "aws_instance" "app_server" {
   ami           = "ami-830c94e3"
